9359d2619f4a45280f4d039f4d8b94e61c53070e
kuldeep singh <kuldeeprathore74@gmail.com>
2022-01-05 00:01:37 +0530

    adjusted remaining pages

--- src/Playlists/PlaylistPlayer/PlaylistPlayer.js
+++ src/Playlists/PlaylistPlayer/PlaylistPlayer.js
@@ -2,82 +2,24 @@ import React from "react"
 import VideoCard from "../../VideoPage/Components/VideoCard/VideoCard"
 
 const PlaylistPlayer = () => {
+  const video = {
+    youtubeId: "1XXVknMiVfc",
+    title: "Never Be Comfortable With Failure | Ankur Warikoo",
+    creator: "Josh Talks",
+    views: "4,393,898",
+  }
   return (
     <div className="videoPage">
       <h2>Playlist Name </h2>
       <div className="videoPage__videos">
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
-        <VideoCard
-          youtubeId="1XXVknMiVfc"
-          title="Never Be Comfortable With Failure | Ankur Warikoo"
-          creator="Josh Talks"
-          views="4,393,898"
-        />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
+        <VideoCard videoObj={video} />
       </div>
     </div>
   )
