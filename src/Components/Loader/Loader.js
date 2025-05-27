4fe225ac68195f6ce3a965280dd02479ba7ae4c2
kuldeep singh <kuldeeprathore74@gmail.com>
2025-05-27 17:19:48 +0530

    fixes

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -4,6 +4,10 @@ import "./Loader.css"
 export const Loader = () => {
   return (
     <div className="loader">
+      <div className=" ">
+        <h1 className="">Please wait for a minute or Retry after 2 min.</h1>
+        <p className="">Server Hosted On Free tier so it takes time</p>
+      </div>
       <img src={loader} alt="loader" />
     </div>
   )
